92c24e3411e713c4412357b8957eb9db1f506fe0
Daniel Reuterwall <daniel@reuterwall.com>
2014-09-20 23:25:42 +0200

    Updated contact form

---
+++ assets/js/mailer.js
@@ -0,0 +1,32 @@
+var bjorg = angular.module('bjorg', []);
+
+bjorg.controller('mailer', ['$scope', function($scope) {
+    Parse.initialize("5DaKMydWmtxbHDBGeNT1LYhnDQTKPibcKs5ObNJf", "aiggeCYClT6opuqsG1BJ65qLRxw55rI8nccmelXS");
+
+    $scope.send = function() {
+      $scope.sending = true;
+      
+      var data = {
+        name: $scope.name,
+        email: $scope.email,
+        phone: $scope.phone,
+        message: $scope.message
+      }
+
+      Parse.Cloud.run("sendEmail", data, {
+        success: function(object) {
+          $scope.$apply(function() {
+            $scope.success = true;
+            $scope.sending = false;
+          });
+        },
+   
+        error: function(object, error) {
+          $scope.$apply(function() {
+            $scope.error = true;
+            $scope.sending = false;
+          });
+        }
+      });
+    }
+}]);
